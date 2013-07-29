4bc3f1fd8b19a66bc615d1c58cdf0f7e5ecebc0d
cesartoii <cesartori_bio@hotmail.com>
2013-07-29 17:44:18 -0500

    crando menu telefono

---
+++ src/main/java/edu/telefono/aplicaciones/Menu.java
@@ -0,0 +1,46 @@
+package edu.telefono.aplicaciones;
+
+import java.util.Scanner;
+import edu.telefono.aplicaciones.*;
+
+public class Menu {
+
+  public void mostrarMenu() {
+		
+		
+
+		Scanner read = new Scanner(System.in);
+		int op = 0;
+
+		do {
+			System.out.println("Menú Principal");
+			System.out.println("1. Contacto " + "\n2. Calculadora"
+					+ "\n3. Musica" + "\n4. Enviar SMS"
+					+ "\n5. Enviar Archivos" + "\n6. Salir");
+			System.out.println("Elija una opción");
+
+			op = read.nextInt();
+
+			if (op == 1) {
+				// ejecutar el Programa Uno
+			}
+
+			else if (op == 2) {
+				// ejecutar el Programa Dos
+			} else if (op == 3) {
+				// ejecutar el Programa Dos
+			} else if (op == 4) {
+				// ejecutar el Programa Dos
+			} else if (op == 5) {
+				// ejecutar el Programa Dos
+			} else if (op == 6) {
+				System.out.println("Gracias por utilizar la aplicación");
+			} else {
+				System.out.println("ERROR, por favor escriba bien");
+			}
+
+		} while (op != 6);
+
+	}
+
+}
