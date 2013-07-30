39ea6243cc32008f8786e623ca6d9c945bf9bbce
AdrianaJuarez <juarez.adriana@hotmail.com>
2013-07-30 11:46:12 -0500

    Creando Metodos Suma, Resta y Multiplicacion
    Creando Metodos Suma, Resta y Multiplicacion

--- src/main/java/edu/telefono/calculadora/Calculadora.java
+++ src/main/java/edu/telefono/calculadora/Calculadora.java
@@ -56,3 +56,157 @@ public class Calculadora {
 
 		}
 	}
+
+
+
+	// Metodo Sumar
+		private void Suma() {
+			try {
+				float sumar;
+				
+				System.out.println("Ingrese primer numero: ");
+				BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+				String frase1 = new String(br1.readLine());
+				float num1 = Float.parseFloat(frase1);
+
+				System.out.println("Ingrese segundo numero: ");
+				BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+				String frase2 = new String(br2.readLine());
+				float num2 = Float.parseFloat(frase2);
+				sumar = num1 + num2;
+				System.out.println("\nEl resultado de la suma es: " + sumar);
+				try{
+					System.out.println("Desea continuar sumando?\n1=si\n2=no");
+					BufferedReader opsi = new BufferedReader(new InputStreamReader(System.in));
+					String pregunta = new String(opsi.readLine());
+					int op1 = Integer.parseInt(pregunta);	
+				while(op1==1) {
+					System.out.println("Ingrese otro numero: ");
+					BufferedReader br3 = new BufferedReader(new InputStreamReader(System.in));
+					String frase3 = new String(br3.readLine());
+					float num3 = Float.parseFloat(frase3);
+					sumar+=num3;
+					
+					System.out.println("\nEl resultado de la suma es: " + sumar);
+					System.out.println("\nDesea continuar sumando?\n1=si\n2=no");
+					opsi = new BufferedReader(new InputStreamReader(System.in));
+					pregunta = new String(opsi.readLine());
+					op1 = Integer.parseInt(pregunta);
+
+				}
+			} catch (Exception error) {
+
+			}
+				
+				Limpiar();
+				System.out.println("\t\tCalculadora");
+				System.out.println("\n\t\t   Suma");
+				System.out.println("\nEl resultado de la suma es: " + sumar);
+				Regresar();
+				
+			} catch (Exception error) {
+				System.out.println("\nSolo se admiten numeros");
+			}
+		}
+
+		// Metodo Restar
+		private void Resta() {
+			
+			try {
+				float resultado;
+				
+				System.out.println("Ingrese primer numero: ");
+				BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+				String frase1 = new String(br1.readLine());
+				float num1 = Float.parseFloat(frase1);
+
+				System.out.println("Ingrese segundo numero: ");
+				BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+				String frase2 = new String(br2.readLine());
+				float num2 = Float.parseFloat(frase2);
+				resultado = num1 - num2;
+				System.out.println("\nEl resultado de la resta es: " + resultado);
+				try{
+					System.out.println("Desea continuar sumando?\n1=si\n2=no");
+					BufferedReader opsi = new BufferedReader(new InputStreamReader(System.in));
+					String pregunta = new String(opsi.readLine());
+					int op1 = Integer.parseInt(pregunta);	
+				while(op1==1) {
+					System.out.println("Ingrese otro numero: ");
+					BufferedReader br3 = new BufferedReader(new InputStreamReader(System.in));
+					String frase3 = new String(br3.readLine());
+					float num3 = Float.parseFloat(frase3);
+					resultado -=num3;
+					
+					System.out.println("\nEl resultado de la resta es: " + resultado);
+					System.out.println("\nDesea continuar sumando?\n1=si\n2=no");
+					opsi = new BufferedReader(new InputStreamReader(System.in));
+					pregunta = new String(opsi.readLine());
+					op1 = Integer.parseInt(pregunta);
+
+				}
+			} catch (Exception error) {
+
+			}
+				
+				Limpiar();
+				System.out.println("\t\tCalculadora");
+				System.out.println("\n\t\t   Resta");
+				System.out.println("\nEl resultado de la resta es: " + resultado);
+				Regresar();
+				
+			} catch (Exception error) {
+				System.out.println("\nSolo se admiten numeros");
+			}
+
+		}
+
+		// Metodo Multiplicar
+		private void Multiplicacion() {
+			try {
+				float resultado;
+				
+				System.out.println("Ingrese primer numero: ");
+				BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+				String frase1 = new String(br1.readLine());
+				float num1 = Float.parseFloat(frase1);
+
+				System.out.println("Ingrese segundo numero: ");
+				BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+				String frase2 = new String(br2.readLine());
+				float num2 = Float.parseFloat(frase2);
+				resultado = num1 * num2;
+				System.out.println("\nEl resultado de la multiplicacion es: " + resultado);
+				try{
+					System.out.println("Desea continuar Multiplicando?\n1=si\n2=no");
+					BufferedReader opsi = new BufferedReader(new InputStreamReader(System.in));
+					String pregunta = new String(opsi.readLine());
+					int op1 = Integer.parseInt(pregunta);	
+				while(op1==1) {
+					System.out.println("Ingrese otro numero: ");
+					BufferedReader br3 = new BufferedReader(new InputStreamReader(System.in));
+					String frase3 = new String(br3.readLine());
+					float num3 = Float.parseFloat(frase3);
+					resultado *=num3;
+					
+					System.out.println("\nEl resultado de la multiplicacion es: " + resultado);
+					System.out.println("\nDesea continuar multiplicando?\n1=si\n2=no");
+					opsi = new BufferedReader(new InputStreamReader(System.in));
+					pregunta = new String(opsi.readLine());
+					op1 = Integer.parseInt(pregunta);
+
+				}
+			} catch (Exception error) {
+
+			}
+				
+				Limpiar();
+				System.out.println("\t\tCalculadora");
+				System.out.println("\n\t\t   Multiplicacion");
+				System.out.println("\nEl resultado de la multiplicacion es: " + resultado);
+				Regresar();
+				
+			} catch (Exception error) {
+				System.out.println("\nSolo se admiten numeros");
+			}
+		}
