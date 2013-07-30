ddcaa0bc3908ce5175b25baada135059fddda851
upastor <uripast_@hotmail.com>
2013-07-30 13:15:18 -0500

    Modificando ViewFiles

--- src/main/java/edu/telefono/vistas/vistaarchivo/ViewFiles.java
+++ src/main/java/edu/telefono/vistas/vistaarchivo/ViewFiles.java
@@ -8,24 +8,22 @@ import java.io.IOException;
 public class ViewFiles {
 	private static FileInputStream fin;
 
-	public static void main(String[] arg){
-		
+	public static void main(String[] arg) {
+
 		File file = new File("C://Prueba//Archivo.txt");
-		
-		try{
+
+		try {
 			fin = new FileInputStream(file);
-			byte fileContent[] = new byte[(int)file.length()];
+			byte fileContent[] = new byte[(int) file.length()];
 			fin.read(fileContent);
-			
+
 			String strFileContent = new String(fileContent);
-			
+
 			System.out.println("File content: ");
 			System.out.println(strFileContent);
-		}
-		catch(FileNotFoundException e){
+		} catch (FileNotFoundException e) {
 			System.out.println("Archivo no encontrado " + e);
-		}
-		catch(IOException ioe){
+		} catch (IOException ioe) {
 			System.out.println("Error al enviar archivo " + ioe);
 		}
 	}
