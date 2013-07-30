646fada787f0e2c283df82c438b1a1b3d538353f
upastor <uripast_@hotmail.com>
2013-07-30 13:03:08 -0500

    Creando la vista Archivo

---
+++ src/main/java/edu/telefono/vistas/vistaarchivo/VistaArchivo.java
@@ -0,0 +1,30 @@
+package edu.telefono.vistas.vistaarchivo;
+
+import java.io.*;
+
+public class VistaArchivo {
+
+	private static FileInputStream fin;
+
+	public static void main(String[] arg){
+		
+		File file = new File("C://Prueba//Archivo.txt");
+		
+		try{
+			fin = new FileInputStream(file);
+			byte fileContent[] = new byte[(int)file.length()];
+			fin.read(fileContent);
+			
+			String strFileContent = new String(fileContent);
+			
+			System.out.println("File content: ");
+			System.out.println(strFileContent);
+		}
+		catch(FileNotFoundException e){
+			System.out.println("Archivo no encontrado " + e);
+		}
+		catch(IOException ioe){
+			System.out.println("Error al enviar archivo " + ioe);
+		}
+	}
+}
