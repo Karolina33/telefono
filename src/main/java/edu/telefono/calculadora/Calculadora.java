85a69f4a18581141ad9555f315652a36ada455bc
AdrianaJuarez <juarez.adriana@hotmail.com>
2013-07-30 19:32:12 -0500

    Modificando Clase
    Modificando Clase

--- src/main/java/edu/telefono/calculadora/Calculadora.java
+++ src/main/java/edu/telefono/calculadora/Calculadora.java
@@ -10,56 +10,7 @@ import java.io.InputStreamReader;
  */
 public class Calculadora {
 
-  public void Menu() {
-		try {
-			System.out.println("\t    Calculadora\n");
-			System.out.println("Elija la operación que desea realizar:");
-			System.out.println("\n\t1\tSuma");
-			System.out.println("\t2\tResta");
-			System.out.println("\t3\tMultiplicacion");
-			System.out.println("\t4\tDivicion");
-			System.out.println("\t5\tRaiz Cuadrada");
-			System.out.println("\t6\tSeno");
-			System.out.println("\t7\tCoseno");
-			System.out.println("\nElija una opción:");
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			String frase = new String(br.readLine());
-			int opcion = Integer.parseInt(frase);
-
-			switch (opcion) {
-			case 1:
-				this.Suma();
-				break;
-			case 2:
-				this.Resta();
-				break;
-			case 3:
-				this.Multiplicacion();
-				break;
-			case 4:
-				this.Division();
-				break;
-			case 5:
-				this.Raiz();
-				break;
-			case 6:
-				this.Seno();
-				break;
-			case 7:
-				this.Coseno();
-				break;
-			default:
-				System.out.println("Opcion incorrecta");
-				break;
-			}
-		} catch (Exception error) {
-
-		}
-	}
-
-
-
-	// Metodo Sumar
+  	// Metodo Sumar
 		private void Suma() {
 			try {
 				float sumar;
@@ -270,7 +221,7 @@ public class Calculadora {
 			}
 		}
 		
-		
+		// Metodo Raiz
 		private void Raiz() {
 			try {
 				double raiz;
@@ -291,6 +242,8 @@ public class Calculadora {
 			}
 		}
 		
+		
+		// Metodo Regresar
 		private void Regresar(){
 				
 			try {
@@ -319,7 +272,9 @@ public class Calculadora {
 			}
 		}
 		
-		
+	
+	// Metodo Coseno
+	
 	private void Coseno() {
 		try {
 			double coseno;
@@ -340,7 +295,7 @@ public class Calculadora {
 		}
 	}
 
-
+	// Metodo Seno
 	private void Seno() {
 
 		try {
@@ -364,7 +319,7 @@ public class Calculadora {
 
 
 
-
+	// Metodo Limpiar
 	private void Limpiar(){
 		int saltos = 10; 
 		for(int i = 0; i < saltos; i++){ 
