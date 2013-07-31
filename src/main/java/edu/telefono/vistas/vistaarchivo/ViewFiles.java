72ba94abeff8034e32845db33d84cfc20125b6ad
Jovanywater <jovany_778@hotmail.com>
2013-07-30 20:37:52 -0500

    Modificando ViewFiles

--- src/main/java/edu/telefono/vistas/vistaarchivo/ViewFiles.java
+++ src/main/java/edu/telefono/vistas/vistaarchivo/ViewFiles.java
@@ -1,30 +1,49 @@
 package edu.telefono.vistas.vistaarchivo;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+
+import edu.telefono.aplicaciones.Menu;
+import edu.telefono.aplicaciones.Telefono;
+import edu.telefono.archivo.EnviarArchivo;
+import edu.telefono.vistas.vistacalculadora.VistaCalculadora;
 
 public class ViewFiles {
-  private static FileInputStream fin;
 
-	public static void main(String[] arg) {
-
-		File file = new File("C://Prueba//Archivo.txt");
-
+	public static void main(String a[]) {
+			EnviarArchivo enviar = new EnviarArchivo();
 		try {
-			fin = new FileInputStream(file);
-			byte fileContent[] = new byte[(int) file.length()];
-			fin.read(fileContent);
+			System.out.println("Intoduzca una opción:");
+			// System.out.println ("---------------------");
+			System.out.println("1. Agregar ruta");
+			System.out.println("R. Regresar al menu principal");
+			System.out.println("S. Salir");
 
-			String strFileContent = new String(fileContent);
-
-			System.out.println("File content: ");
-			System.out.println(strFileContent);
-		} catch (FileNotFoundException e) {
-			System.out.println("Archivo no encontrado " + e);
-		} catch (IOException ioe) {
-			System.out.println("Error al enviar archivo " + ioe);
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					System.in));			
+			String frase = new String(br.readLine());
+			char opcion = frase.charAt(0);
+			switch (opcion) {
+			case '1':
+				System.out.println("Insertar Ruta");
+				enviar.enviandoArchivo("/C:/Prueba/Archivo.txt");
+				break;
+			case 'R':
+			case 'r':
+				Menu menu = new Menu();
+				menu.mostrarMenu();
+				break;
+			case 'S':
+			case 's':
+				System.exit(0);
+			default:
+				System.out.println("Opcion incorrecta");
+				break;
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
