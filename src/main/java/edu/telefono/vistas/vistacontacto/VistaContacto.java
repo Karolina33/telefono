d381efb205a5a9d4fc5ef99f6c33e0f0106ca25d
Luis Enrique Angeles Mendieta <le.angeles.m@hotmail.com>
2013-07-31 23:19:12 -0500

    Modificacion menu

--- src/main/java/edu/telefono/vistas/vistacontacto/VistaContacto.java
+++ src/main/java/edu/telefono/vistas/vistacontacto/VistaContacto.java
@@ -10,8 +10,8 @@ public class VistaContacto
   public void vistacontacto() {
 		Agenda agenda=new Agenda();
 		Telefono tel = new Telefono();
-		String opcion="";
-		while (opcion!="S")
+		int opcion=1;
+		while (opcion!=0)
 		{
 			System.out.println(" ");
 			System.out.println("Menu Agenda");
@@ -20,38 +20,38 @@ public class VistaContacto
 			System.out.println("3 Buscar Contacto por telefono");
 			System.out.println("4 Modificar Contacto por nombre");
 			System.out.println("5 Eliminar contacto por Nombre");
-			System.out.println("R Regresar");
-			System.out.println("S Salir");
+			System.out.println("6 Regresar");
+			System.out.println("0 Salir");
 			opcion=menu();
 			
 				switch (opcion){
-				case "S":
-					System.out.println("Saliendo de Contactos");
+				case 0:
+					System.out.println("Saliendo de telefono");
 					System.exit(0);
 					/*Cierra telefono*/
 					break;
-				case "R":
+				case 6:
 					System.out.println("Regresando a Telefono");
 					tel.menuPrincipal();
 					/*aqui se redirecciona a la vista principal*/
 					break;	
-				case "1":
+				case 1:
 					agenda.agregar();
 					break;
 			
-				case "2":
+				case 2:
 					agenda.busquedanom();
 					break;
 				
-				case "3":			
+				case 3:			
 					agenda.busquedatel();
 					break;
 		
-				case "4":
+				case 4:
 					agenda.modnombre();
 					break;
 		
-				case "5":
+				case 5:
 					agenda.elimnom();
 					break;		
 				default:
@@ -63,14 +63,14 @@ public class VistaContacto
 		
 }
 	}
-	static String menu()
+	static int menu()
 	{
 		InputStreamReader is = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(is);
-		String opcion="";
+		int opcion=1;
 		try
 		{
-			opcion = br.readLine();
+			opcion = Integer.parseInt(br.readLine());
 		}
 		catch(IOException e)
 		{
